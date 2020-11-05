da3e00cd46c242631f59798e9b06b0c692671f94
k.nebogin <kinebogin@gmail.com>
2020-11-06 01:54:46 +0300

    Rewrote crawling process to save block_headers, leases and lease_cancel_txs (just in case). The leases json now contains 'activation_height' and optionally 'cancel_height' with 'lease_cancel_id', if it was cancelled.

--- PayoutCalculator.py
+++ PayoutCalculator.py
@@ -3,6 +3,7 @@ import requests
 import sys
 from pymongo import MongoClient, ASCENDING, DESCENDING
 import copy
+import logging
 
 balance_depth = 1000
 
@@ -74,16 +75,12 @@ class LeaserProfile:
 
 
 """
-    Okay, well. Python, khem.
-    1. Input data: miner's address
-    2. Skip blocks until we find the first one made by given miner, that's when we start the count
-    3. In every block look for Lease (type 8) txs towards miner (miner should be recipient)
-        Save: sender, amount, leaseId, height
-    4. In every block look for LeaseCancel (type 9) txs for LeaseTxs (by leaseId)
-        Save: sender, leaseId, height
-    5. For every block:
-        * save it's fee in case next one would be miner's block
-        * 
+    A script to crawl the blockchain and calculate leaser payouts.
+    Command arguments:
+        * Node REST API URL, to be used for crawling;
+        * Miner address as a Base58 encoded string;
+        ? Command to execute. One of: ['crawl', 'check', 'calculate']
+    (Arguments marked with '?' are optional)
 """
 
 
@@ -92,48 +89,53 @@ step = 100
 LEASE_TX_TYPE = 8
 LEASE_CANCEL_TX_TYPE = 9
 
-PreparedDataToPersist = namedtuple('PreparedDataToPersist', 'block_headers, lease_txs, lease_cancel_txs')
-
 
 def get_blocks(node_api, from_block, to_block):
     return requests.get(node_api + '/blocks/seq/{}/{}'.format(from_block, to_block)).json()
 
 
-def add_height_to_txs(txs, height):
+def enrich_tx_json(txs, height):
     for tx in txs:
         tx.update({'height': height})
+        if tx['type'] == LEASE_TX_TYPE:
+            tx.update({'activation_height': height + balance_depth})
         yield tx
 
 
-def prepare_data_to_persist(blocks):
+CrawledData = namedtuple('CrawledData', 'block_headers, lease_txs, lease_cancel_txs')
+
+
+def extract_crawled_data(blocks):
+    """
+        Every block is torn apart into block headers, lease txs and lease_cancel txs
+        Method returns a sequence of CrawledData objects
+    """
     block_headers = []
     lease_txs_accumulator = []
     lease_cancel_txs_accumulator = []
     for block in blocks:
-        # block_header = block.pop('transactions', None)
         height = block['height']
         block_txs = copy.deepcopy(block['transactions'])
         filtered_lease_txs = filter(lambda tx: tx['type'] == LEASE_TX_TYPE, block_txs)
-        lease_txs = add_height_to_txs(filtered_lease_txs, height)
+        lease_txs = enrich_tx_json(filtered_lease_txs, height)
         filtered_lease_cancel_txs = filter(lambda tx: tx['type'] == LEASE_CANCEL_TX_TYPE, block_txs)
-        lease_cancel_txs = add_height_to_txs(filtered_lease_cancel_txs, height)
+        lease_cancel_txs = enrich_tx_json(filtered_lease_cancel_txs, height)
         lease_txs_accumulator.extend([x for x in lease_txs])
         lease_cancel_txs_accumulator.extend([x for x in lease_cancel_txs])
         block.pop('transactions', None)
         block_headers.append(block)
-    return PreparedDataToPersist(block_headers, lease_txs_accumulator, lease_cancel_txs_accumulator)
+    return CrawledData(block_headers, lease_txs_accumulator, lease_cancel_txs_accumulator)
 
 
 def check_index(db_collection, index_name, direction):
     if any(index_key for index_key in db_collection.index_information().keys() if index_key.startswith(index_name)):
-        print("Index '{}' is present for collection '{}'".format(index_name, db_collection.name))
+        logger.info("Index '{}' is present for collection '{}'".format(index_name, db_collection.name))
     else:
         db_collection.create_index([(index_name, direction)])
-        print("Created index '{}' for collection '{}'".format(index_name, db_collection.name))
+        logger.info("Created index '{}' for collection '{}'".format(index_name, db_collection.name))
 
 
 def find_leases(txs, height, miner):
-    # for lease in ():
     return [LeaseTx(tx['sender'], tx['amount'], tx['id'], height) for tx in txs if(tx['type'] == LEASE_TX_TYPE and tx['recipient'] == miner)]
 
 
@@ -146,21 +148,25 @@ def total_stake_at_height(profiles, height):
 
 
 if __name__ == '__main__':
-    print('Payout calculator launched')
+    logging.basicConfig(
+        level=logging.INFO, format='%(asctime)s %(levelname)s [%(module)s] %(message)s')
+    logger = logging.getLogger('PayoutCalculator')
+
+    logger.info('Payout calculator launched')
     arg_names = ['cmd', 'node_api', 'miner', 'maybe_command']
     named_args = dict(zip(arg_names, sys.argv))
     if named_args.get('node_api') is None:
-        print('Node api not specified')
+        logger.error('Node api not specified')
         sys.exit(2)
     node_api = named_args.get('node_api')
 
     if named_args.get('miner') is None:
-        print('Miner is not defined')
+        logger.error('Miner is not defined')
         sys.exit(2)
     miner = named_args.get('miner')
 
     if named_args.get('maybe_command') is None:
-        print('What do you want to do?', 'Type one of the following commands:', 'crawl', 'check', 'calculate', sep='\n')
+        logger.info('What do you want to do?', 'Type one of the following commands:', 'crawl', 'check', 'calculate', sep='\n')
         command = input("On your command: ")
     else:
         command = named_args.get('maybe_command')
@@ -170,7 +176,7 @@ if __name__ == '__main__':
     db = db_client.payout_calculator
 
     if command == 'crawl':
-        print('Starting the crawler')
+        logger.info('Starting the crawler')
         crawl_start_height = 1
 
         check_index(db.block_headers, 'height', ASCENDING)
@@ -181,43 +187,54 @@ if __name__ == '__main__':
             crawl_start_height = db.block_headers.find().sort([('height', DESCENDING)]).next()['height'] + 1
 
         blockchain_height = requests.get('{}/blocks/height'.format(node_api)).json()['height']
-        print("Initiating crawling process from '{}' to '{}'".format(crawl_start_height, blockchain_height))
+        logger.info("Initiating crawling process from '{}' to '{}'".format(crawl_start_height, blockchain_height))
 
         for seq_start in range(crawl_start_height, blockchain_height, step):
             seq_end = seq_start + step - 1
-            print('Crawling blocks from {} to {} ({} left)'.format(seq_start, seq_end, blockchain_height - seq_start))
+            logger.debug('Crawling blocks from {} to {} ({} left)'.format(seq_start, seq_end, blockchain_height - seq_start))
             blocks = get_blocks(node_api, seq_start, seq_end)
-            prepared_data = prepare_data_to_persist(blocks)
+            crawled_data = extract_crawled_data(blocks)
 
-            db.block_headers.insert_many(prepared_data.block_headers)
-            db.lease_txs.insert_many(prepared_data.lease_txs)
-            db.lease_cancel_txs.insert_many(prepared_data.lease_cancel_txs)
-        print('Crawling done')
+            db.block_headers.insert_many(crawled_data.block_headers)
+            db.leases.insert_many(crawled_data.lease_txs)
+            db.lease_cancel_txs.insert_many(crawled_data.lease_cancel_txs)
+
+            # Process lease cancels one by one, updating leases with 'cancel_height' and 'lease_cancel_id'
+            for lease_cancel in crawled_data.lease_cancel_txs:
+                lease_id = lease_cancel['leaseId']
+                logger.debug("Updating cancelled lease with id '{}'".format(lease_id))
+                db.leases.update_one({"id": lease_id},
+                                     {"$set": {
+                                         "cancel_height": lease_cancel['height'],
+                                         "lease_cancel_id": lease_cancel['id']
+                                     }})
+
+        logger.info('Crawling done')
         sys.exit()
 
     elif command == 'check':
         crawled_blocks_count = db.blocks.count_documents({})
-        print('There are {} blocks in Mongo DB'.format(crawled_blocks_count))
+        logger.info('There are {} blocks in Mongo DB'.format(crawled_blocks_count))
         last_block = db.blocks.find().sort([('height', DESCENDING)]).limit(1).next()
-        print("Last block's height is {}".format(last_block['height']))
-        print('Establishing backwards chain check starting from {}'.format(last_block['signature']))
+        logger.info("Last block's height is {}".format(last_block['height']))
+        logger.info('Establishing backwards chain check starting from {}'.format(last_block['signature']))
         while last_block['height'] > 1:
             parent_block_id = last_block['reference']
             maybe_parent_block = db.blocks.find_one({'signature': parent_block_id})
             if maybe_parent_block is None:
                 raise Exception("Couldn't find block '{}' for height {}".format(parent_block_id, last_block['height']))
             else:
-                print('Found previous block {} at height {}'.format(maybe_parent_block['signature'], maybe_parent_block['height']))
+                logger.debug('Found previous block {} at height {}'.format(maybe_parent_block['signature'], maybe_parent_block['height']))
                 last_block = maybe_parent_block
-        print('Block storage check success: reached genesis block')
+        logger.info('Block storage check success: reached genesis block')
 
     elif command == 'calculate':
         since_height = 1
         since_height_input = input("Calculate since (height, default is 1): ")
         if len(since_height_input) == 0:
-            print('Going to calculate since genesis')
+            logger.info('Going to calculate since genesis')
         elif not since_height_input.isdigit():
-            print('Expected height as a number, aborting')
+            logger.error('Expected height as a number, aborting')
             sys.exit(2)
         else:
             since_height = int(since_height_input)
@@ -225,9 +242,9 @@ if __name__ == '__main__':
         period_start_height = since_height
         period_start_height_input = input("Enter start period height: ")
         if len(period_start_height_input) == 0:
-            print('Going to calculate payouts since {} height'.format(since_height))
+            logger.info('Going to calculate payouts since {} height'.format(since_height))
         elif not period_start_height_input.isdigit():
-            print('Expected height as a number, aborting')
+            logger.error('Expected height as a number, aborting')
             sys.exit(2)
         else:
             period_start_height = int(period_start_height_input)
@@ -235,15 +252,15 @@ if __name__ == '__main__':
         reward_coef_input = input("Enter reward coefficient (default is 0.9): ")
         reward_coef = 0.9
         if len(reward_coef_input) == 0:
-            print('Going to calculate using reward coefficient of 0.9 (90% earnings for leasers)')
+            logger.info('Going to calculate using reward coefficient of 0.9 (90% earnings for leasers)')
         else:
             try:
                 reward_coef = float(reward_coef_input)
             except ValueError:
-                print('ERROR: expected reward_coef as a float number')
+                logger.error('Expected reward_coef as a float number')
                 sys.exit(2)
 
-        print('Launching calculation since height {}'.format(since_height))
+        logger.debug('Launching calculation since height {}'.format(since_height))
         known_leases = []
         known_lease_ids = []
         cancelled_leases = []
@@ -263,7 +280,7 @@ if __name__ == '__main__':
                 no_cursor_timeout=True,
                 session=db_session).sort([('height', ASCENDING)])
             blocks_to_process = found_blocks_cursor.count()
-            print('Got {} blocks to process'.format(blocks_to_process))
+            logger.info('Got {} blocks to process'.format(blocks_to_process))
             iteration_count = 1
             for block in found_blocks_cursor:
                 txs = block['transactions']
@@ -271,7 +288,7 @@ if __name__ == '__main__':
 
                 # Update mongo session every 100th block processed
                 if iteration_count % 100 == 0:
-                    print('Iteration {}/{}, processing block at height {}'
+                    logger.debug('Iteration {}/{}, processing block at height {}'
                           .format(iteration_count, blocks_to_process, block_height))
                     session_update_result = db_client.admin\
                         .command('refreshSessions', [db_session.session_id], session=db_session)
@@ -280,12 +297,12 @@ if __name__ == '__main__':
                 if len(leases) > 0:
                     known_leases.extend(leases)
                     known_lease_ids.extend(list(map(lambda l: l.lease_id, leases)))
-                    print('Found leases:', *leases, sep='\n')
+                    logger.debug('Found leases:', *leases, sep='\n')
 
                 cancelled = find_canceled_leases(txs, known_lease_ids, block_height)
                 if len(cancelled) > 0:
                     cancelled_leases.extend(cancelled)
-                    print('Found cancelled leases:', *cancelled, sep='\n')
+                    logger.debug('Found cancelled leases:', *cancelled, sep='\n')
 
                 if block['generator'] == miner and block['height'] >= period_start_height:
                     block_fee = block['fee']
@@ -302,13 +319,13 @@ if __name__ == '__main__':
 
                 iteration_count += 1
 
-        print('Found leases:', *known_leases, sep='\n')
-        print('Cancelled leases:', *cancelled_leases, sep='\n')
-        print('Mined blocks with non-zero fees:', *mined_blocks, sep='\n')
-        print('Total earned fees: ', sum(map(lambda b: b.calculated_fees, mined_blocks)))
+        logger.debug('Found leases:', *known_leases, sep='\n')
+        logger.debug('Cancelled leases:', *cancelled_leases, sep='\n')
+        logger.debug('Mined blocks with non-zero fees:', *mined_blocks, sep='\n')
+        logger.debug('Total earned fees: ', sum(map(lambda b: b.calculated_fees, mined_blocks)))
 
         senders = set(map(lambda l: l.sender, known_leases))
-        print('{} unique leasers'.format(len(senders)))
+        logger.debug('{} unique leasers'.format(len(senders)))
 
         leaser_profiles = []
         for sender in senders:
@@ -318,31 +335,31 @@ if __name__ == '__main__':
             profile = LeaserProfile(sender, all_leases, all_canceled)
             leaser_profiles.append(profile)
 
-        print('Constructed leaser profiles:', *leaser_profiles, sep='\n')
+        logger.debug('Constructed leaser profiles:', *leaser_profiles, sep='\n')
         max_height_in_db = db.blocks.find().sort([('height', DESCENDING)]).limit(1).next()['height']
         stake_at_last_height = sum(map(lambda p: p.stake_for_height(max_height_in_db), leaser_profiles))
-        print('Check the stake at last crawled height {}: {}'.format(max_height_in_db, stake_at_last_height))
+        logger.debug('Check the stake at last crawled height {}: {}'.format(max_height_in_db, stake_at_last_height))
 
         payouts = defaultdict(lambda: 0)
         for block in mined_blocks:
             height = block.height
             total_stake = total_stake_at_height(leaser_profiles, height)
-            print('For block mined at height {} with total stake {}'.format(height, total_stake))
+            logger.debug('For block mined at height {} with total stake {}'.format(height, total_stake))
             for leaser in leaser_profiles:
                 leased_at_height = leaser.stake_for_height(height)
                 if leased_at_height > 0:
                     reward_for_height = round(reward_coef * block.calculated_fees * (leased_at_height / total_stake))
-                    print("\tLeaser '{}' gets {} reward".format(leaser.sender, reward_for_height))
+                    logger.debug("\tLeaser '{}' gets {} reward".format(leaser.sender, reward_for_height))
                     payouts[leaser.sender] += reward_for_height
 
-        print('Here are the overall payouts for height {}'.format(max_height_in_db))
+        logger.info('Here are the overall payouts for height {}'.format(max_height_in_db))
         for leaser_address, payout_amount in payouts.items():
-            print("Leaser '{}' -> {}".format(leaser_address, payout_amount))
-        print('Total payouts: {}'.format(sum(payouts.values())))
+            logger.info("Leaser '{}' -> {}".format(leaser_address, payout_amount))
+        logger.info('Total payouts: {}'.format(sum(payouts.values())))
 
         left_leasers = [x.sender for x in leaser_profiles if x.stake_for_height(max_height_in_db) == 0]
-        print('Left leasers: ', *left_leasers, sep='\n')
-        print('Calculation done')
+        logger.info('Left leasers: ', *left_leasers, sep='\n')
+        logger.info('Calculation done')
         sys.exit()
 
     else:
